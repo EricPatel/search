613643e0861436133361e2c1ffa5f0ab7d39e324
EricPatel <EricPatelOfficial@gmail.com>
2020-02-01 13:13:16 -0500

    moved methods to appropriate modules

--- search.py
+++ search.py
@@ -1,36 +1,4 @@
-import random
-import math
 import heapq
-
-# Helper Function used to print graphs in a pretty way
-def printMap(graph):
-    for i in range(len(graph)):
-        for j in range(len(graph)):
-            print(graph[i][j], end=" ")
-        print("")
-
-# Generates a map given a dimension and a probability that a cell is blocked
-# 1 --> Cell is Blocked, 0 --> Cell is Open
-def generateMap(dim, p):
-    graph = []
-
-    # These two loops are used initialize the graph with the correct values
-    for i in range(dim):
-        row = []
-        for j in range(dim):
-            # Random.choices function is an inbuilt python function that takes 
-            # a list of choices and respective weights that returns a choice.
-            # In this case, our two choices are 1 for a cell being blocked and 
-            # 0 for a cell being clear/open. 
-            randomNum = random.choices(population=[1, 0], weights=[p, 1 - p], k=1)[0]
-            row.append(randomNum)
-        graph.append(row)
-
-    # Ensures that the source cell (0,0) is open and the goal cell (dim - 1, dim - 1)
-    # is open, as they may have been initialized to 1 in the previous for loops
-    graph[0][0] = 0
-    graph[dim-1][dim-1] = 0
-    return graph
 
 # Helper function to check if a certain point is between 0 and the graphs dimensions
 def checkPoint(x, y, dim):
