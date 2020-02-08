8d0b163942e097cd944ccc71cbbec9ec0e65659a
EricPatel <EricPatelOfficial@gmail.com>
2020-02-08 16:51:54 -0500

    master changes

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import search
 
 def main():
     # Example graph generation with dimension and probability
-    g = map.generateMap(10, 0.3)
-
+    g = map.generateMap(5, 0.2)
+    g = [[0,0,0,0,0],[0,0,0,0,0],[0,1,0,0,0],[0,1,0,1,1],[0,1,0,0,0]]
     # Path is returned using the graph g
     # The function can be replaced with aStarWithEuclidean, dfs, and aStarWithManhattan
     path = search.aStarWithManhattan(g)
     path2 = search.aStarWithEuclidean(g)
     dfsPath = search.dfs(g)
     bfsPath = search.bfs(g)
-    biBFSPath = search.bidirectionalBfs(g)
     # Easy way to see the path taken by the algorithm used above
+    print(len(path))
     map.printPath(path, g)
     print("")
+    print(len(path2))
     map.printPath(path2, g)
     print("")
-    map.printPath(dfsPath, g)
-    print("")
+    #map.printPath(dfsPath, g)
+    #print("")
+    print(len(path2))
     map.printPath(bfsPath, g)
-    print("")
-    map.printPath(biBFSPath, g)
 
 if __name__ == "__main__":
     main()
