339bb8648a14af43abf55309794ac57316de9171
Eric Patel <ericpatelofficial@gmail.com>
2020-02-16 19:09:02 -0500

    added new firebfs

--- fire.py
+++ fire.py
@@ -8,7 +8,7 @@ def fireTimeStep(graph, q):
     fireGraph = deepcopy(graph)
     dim = len(graph)
 
-    # Iterate through the graph and if a cell is open, we need to evaluate 
+    # Iterate through the graph and if a cell is open, we need to evaluate
     # whether it should be on fire or not.
     for i in range(dim):
         for j in range(dim):
@@ -25,14 +25,14 @@ def fireTimeStep(graph, q):
                     if search.checkPoint(x, y, dim) and graph[x][y] == 2:
                         burningNeighbors = burningNeighbors + 1
 
-                # Generate the probability that a cell is on fire using 
+                # Generate the probability that a cell is on fire using
                 # equation in assignment: 1 - (1 - q)^k
                 pFire = 1 - ((1 - q)**burningNeighbors)
-                randomNum = random.choices(population=[2, 0], weights=[pFire, 1 - pFire], k=1)[0] 
+                randomNum = random.choices(population=[2, 0], weights=[pFire, 1 - pFire], k=1)[0]
                 fireGraph[i][j] = randomNum
     return fireGraph
 
-# Generates the shortest path on the map and traverses the graph, 
+# Generates the shortest path on the map and traverses the graph,
 # while spreading the fire every time step. If the fire reaches one of
 # cells we are on, than we return failure, otherwise we return the path.
 def strategy1(graph, q):
@@ -54,8 +54,8 @@ def strategy1(graph, q):
     return path, graph
 
 # Generates the shortest path on the map every time step and traverses the
-# new shortest path while spreading the fire every time step. 
-# If the fire reaches one of cells we are on, than we return failure, 
+# new shortest path while spreading the fire every time step.
+# If the fire reaches one of cells we are on, than we return failure,
 # otherwise we return the path.
 def strategy2(graph, q):
     path = []
@@ -66,7 +66,7 @@ def strategy2(graph, q):
     # Recalculate shortest path until the final move is on the goal cell
     while point != (dim-1, dim-1):
         result = search.aStarForStrategy2(point, graph, search.euclideanH)
-        
+
         # If there is no path, than we immediately return failure
         if result == "Failure: No Path":
             return "Failure: No Path"
@@ -87,3 +87,45 @@ def strategy2(graph, q):
 # Needs to be done
 def strategy3(graph, q):
     return None
+
+def distanceToFire(graph, curr):
+    dim = len(graph)
+    queue = deque()
+
+    # Create a visited array of same dimensions as the graph to ensure that BFS
+    # will minimize the fringe size
+    visited = [[False for p in range(dim)] for k in range(dim)]
+
+
+    # Enqueue the starting position and mark it as visited
+    queue.append(curr)
+    visited[curr[0]][curr[1]] = True
+
+    distance = 0
+
+    # Keep looping while there are elements in the queue
+    while len(queue) != 0:
+        # Get the first item on the queue
+        point = queue.popleft()
+        x = point[0]
+        y = point[1]
+
+        # Generate a list of all possible neighboring points from the current point (x,y)
+        neighbors = [(x, y-1), (x,y+1), (x-1, y), (x+1, y)]
+        for neighbor in neighbors:
+            i = neighbor[0]
+            j = neighbor[1]
+
+            # If we have reached the goal cell, we can return the path associated with
+            # that cell.
+            if graph[i][j] = 2:
+                return count
+
+            # Only append points on the stack if the points are within the bounds
+            # of the graph, the point is a 0, and the point has not been visited
+            if checkPoint(i, j, dim) and graph[i][j] == 0 and visited[i][j] == False:
+                queue.append([i, j])
+                visited[i][j] = True
+        count += 1
+    # If there is no path from source cell to goal cell than return the string below
+    return "Failure: No Path"
