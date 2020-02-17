b345115093ef46bc6191658ee1bd3a47d0d705b3
shreybohra <shreybohra@gmail.com>
2020-02-16 20:34:59 -0500

    ch

--- fire.py
+++ fire.py
@@ -2,6 +2,11 @@ import random
 import map
 from copy import deepcopy
 import search
+import sys
+import heapq
+from HeapNode import HeapNode
+from collections import deque
+
 
 # Generates a map given a dimension and a probability that a cell is blocked
 # 1 --> Cell is Blocked, 0 --> Cell is Open , 2 --> Cell is on Fire
@@ -119,3 +124,143 @@ def strategy2(graph, q):
         path.append(point)
 
     return path, graph
+
+def strategy3(graph, q):
+    path = []
+    point = (0,0)
+    path.append(point)
+    dim = len(graph)
+
+    # Recalculate shortest path until the final move is on the goal cell
+    while point != (dim-1, dim-1):
+        result = aStarNew(point,graph, heu)
+
+        # If there is no path, than we immediately return failure
+        if result == "Failure: No Path":
+            return "Failure: No Path"
+
+        point = result[1]
+        i = point[0]
+        j = point[1]
+
+        # If the current cell we are on is on fire, than we return failure
+        if graph[i][j] == 2:
+            return "Failure: No Path"
+
+        graph = fireTimeStep(graph, q)
+        #map.printMap(g)
+        #print("")
+        path.append(point)
+
+    return path, graph
+
+def distanceToFire(graph, curr):
+    dim = len(graph)
+    queue = deque()
+
+    # Create a visited array of same dimensions as the graph to ensure that BFS
+    # will minimize the fringe size
+    visited = [[False for p in range(dim)] for k in range(dim)]
+
+    # Enqueue the starting position and mark it as visited
+    queue.append((curr, 0))
+    visited[curr[0]][curr[1]] = True
+
+    # Keep looping while there are elements in the queue
+    while len(queue) != 0:
+        # Get the first item on the queue
+        point = queue.popleft()
+        node = point[0]
+        x = node[0]
+        y = node[1]
+        distance = point[1]
+
+        if graph[x][y] == 2:
+            return distance
+        else:
+            # Generate a list of all possible neighboring points from the current point (x,y)
+            neighbors = [(x, y-1), (x,y+1), (x-1, y), (x+1, y)]
+            for neighbor in neighbors:
+                i = neighbor[0]
+                j = neighbor[1]
+
+                # Only append points on the stack if the points are within the bounds
+                # of the graph, the point is a 0, and the point has not been visited
+                if search.checkPoint(i, j, dim) and graph[i][j] != 1 and visited[i][j] == False:
+                    queue.append(((i, j), distance + 1))
+                    visited[i][j] = True
+
+    # If there is no path from source cell to goal cell than return the string below
+    return "Failure: No Path"
+
+def heu(p1, p2, graph):
+    distToFire = distanceToFire(graph, p1)
+    manDist = search.manhattanH(p1, p2)
+    return manDist - distToFire
+
+# Generates a path from the source cell (0,0) to goal cell (dim - 1, dim - 1) using
+# A* where heuristicMethod can be euclideanH or manhattanH
+def aStarNew(source_cell, graph, heuristicMethod):
+    dim = len(graph)    
+    goal_cell = (dim-1, dim-1)
+    
+    # Dictionary where the key is the cell and the value is the 
+    # cell that was previous. This is used to generate the actual path. 
+    prevMap = {}
+
+    # Create a visited array of same dimensions as the graph which will make sure
+    # that A* considers only cells that have not been visited which will reduce 
+    # the maximum fringe size and prevent cycles. Every position is initialized
+    # to the maximum integer to make sure the correct path is found. 
+    visited = [[sys.maxsize for p in range(dim)] for k in range(dim)]
+
+    # Create a min-heap/priority queue to use for A*
+    heap = []
+    heapq.heapify(heap)
+
+    # Start by appending the estimated distance from the source cell to the goal cell,
+    # the source cell, the previous cell, and the distance from the source.
+    # This heap will automatically use the first value in the tuple to sort the items
+    # because of the __lt__ method in our HeapNode class.
+    first_node = HeapNode(heuristicMethod(source_cell, goal_cell, graph), source_cell, None, 0)
+    
+    prevMap[source_cell] = None
+    heapq.heappush(heap, first_node)
+    visited[source_cell[0]][source_cell[1]] = heuristicMethod(source_cell, goal_cell, graph)
+
+    while len(heap) != 0:
+        node = heapq.heappop(heap)
+        point = node.cell
+        x = point[0]
+        y = point[1]
+
+        # If we have reached the goal cell, we can return the path associated with
+        # that cell.
+        if x == dim - 1 and y == dim - 1:
+            return search.getPath(prevMap, goal_cell)
+        else:
+            # Generate a list of all possible neighboring points from the current point (x,y)
+            points = [(x, y-1), (x,y+1), (x-1, y), (x+1, y)]
+            for (i,j) in points:
+                if search.checkPoint(i, j, dim):
+
+                    # The distance from the source to the current point (i,j)
+                    neighborToSource = node.distFromSource + 1
+
+                    # The estimated distance from the neighbor to the goal cell
+                    neighborPointHeuristic = heuristicMethod((i,j), goal_cell, graph)
+
+                    totalDistanceToGoal = neighborToSource + neighborPointHeuristic
+
+                    # Only append points on the heap if the points are within the bounds
+                    # of the graph, the point is a 0, and the point has a smaller total distance
+                    # than visited[i][j].
+                    if graph[i][j] == 0 and visited[i][j] > totalDistanceToGoal:
+                        visited[i][j] = totalDistanceToGoal
+                        prevMap[(i,j)] = point
+                        neighbor = HeapNode(totalDistanceToGoal, (i,j), point, neighborToSource)
+                        heapq.heappush(heap, neighbor)
+                    
+    # If there is no path from source cell to goal cell than return the string below
+    return "Failure: No Path"
+
--- fireTestMain.py
+++ fireTestMain.py
@@ -2,28 +2,36 @@ import fire
 import map
 
 def main():    
-    number_of_runs = 50
-    dim = 20
+    number_of_runs = 20
+    dim = 10
     p = 0.3
     for q in range(0, 110, 10):
         strategy1_successes = 0
         strategy2_successes = 0
+        strategy3_successes = 0
         for i in range(number_of_runs):
             fireG = fire.generateFireMap(dim, p)
             result1 = fire.strategy1(fireG, q/100)
             result2 = fire.strategy2(fireG, q/100)
+            result3 = fire.strategy3(fireG, q/100)
 
             if result1 != "Failure: No Path":
                 strategy1_successes = strategy1_successes + 1
 
             if result2 != "Failure: No Path":
                 strategy2_successes = strategy2_successes + 1
+            
+            if result3 != "Failure: No Path":
+                strategy3_successes = strategy3_successes + 1
 
         print("Strategy 1 with q = " + str(q/100))
         print("Average Success Rate: " + str(strategy1_successes/number_of_runs))
         print("")
         print("Strategy 2 with q = " + str(q/100))
         print("Average Success Rate: " + str(strategy2_successes/number_of_runs))
+        print("")
+        print("Strategy 3 with q = " + str(q/100))
+        print("Average Success Rate: " + str(strategy3_successes/number_of_runs))
         print("")
 
 if __name__ == "__main__":
